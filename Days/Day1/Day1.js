9cdd06e65d3a16ef8d6528b227a4b735d4296df1
jestro <jestror123@gmail.com>
2024-01-04 15:03:13 +0100

    Finished the exercise.

--- Days/Day1/Day1.js
+++ Days/Day1/Day1.js
@@ -1,7 +1,22 @@
-const string = prompt();
+import {string} from "./string.js"
 
-console.log(getNumbersOfLines(string));
+const numbers = getNumbersOfLines(string);
 
+console.log(sumArray(numbers));
+
+// Sums all items of an array (with parseInt())
+function sumArray(array) {
+    let sum = 0;
+
+    for (const number of array) {
+        sum += parseInt(number);
+    }
+
+    return sum;
+}
+
+
+// Puts first and last number together of each line (into an array)
 function getNumbersOfLines(string) {
     const lines = string.split("\n");
     const numbers = [];
@@ -13,18 +28,28 @@ function getNumbersOfLines(string) {
     return numbers
 }
 
+
+// -- FIND NUMBER --
+
+// Finds first number
+// 7mhjsq7ninetwo3tbnkglngltwo -> 7
 function findFirstNumber(string) {
     for (let i = 0; i < string.length; i++) {
         if (!isNaN(parseInt(string[i]))) {
             return string[i];
         }
     }
+    return '0';
 }
 
+
+// Finds last number
+// 7mhjsq7ninetwo3tbnkglngltwo -> 3
 function findLastNumber(string) {
     for (let i = string.length; i >= 0; i--) {
         if (!isNaN(parseInt(string[i]))) {
             return string[i];
         }
     }
+    return '0';
 }
