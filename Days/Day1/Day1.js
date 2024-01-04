ffb301fd5e3803f3759238b0c8060768ad9b764e
jestro <jestror123@gmail.com>
2024-01-04 14:08:59 +0100

    Made a function that gets the number of every line.

--- Days/Day1/Day1.js
+++ Days/Day1/Day1.js
@@ -1,5 +1,18 @@
 const string = prompt();
 
+console.log(getNumbersOfLines(string));
+
+function getNumbersOfLines(string) {
+    const lines = string.split("\n");
+    const numbers = [];
+
+    for (const line of lines) {
+        numbers.push(findFirstNumber(line) + findLastNumber(line));
+    }
+
+    return numbers
+}
+
 function findFirstNumber(string) {
     for (let i = 0; i < string.length; i++) {
         if (!isNaN(parseInt(string[i]))) {
